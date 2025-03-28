dc1ced4c5967bbe130b298a0ef5a57883bf034b1
Nayaka Fadhila Akbar <kakaphotography12@gmail.com>
2025-03-28 09:11:01 +0700

    update
    Signed-off-by: Nayaka Fadhila Akbar <kakaphotography12@gmail.com>

--- looplat1.py
+++ looplat1.py
@@ -1,6 +1,11 @@
 a = int(input("masukan angka pertama: "))
 b = int(input("masukan angka kedua: "))
 
+'''
+in = format dari for
+range= untuk meenghasilkan, dimulai dari angka pertama sampai angka kedua.
+'''
+
 for i in range(a, b + 1):
     if i % 2 != 0:
         print(i)
